2938482df2672925c497476dab1cdb5572b90be7
tung.buithanh <tung.buithanh@pixta.co.jp>
2017-08-17 13:14:44 +0700

    fix to deploy

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
   res.render('index.html');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server start on port 3000");
 })
